331481d4bcc35d8c21268d124ad0cd56638dedfe
pjh456 <147148383@qq.com>
2026-01-16 16:20:24 +0800

    fix: 修复了项目根目录导入模块方式

--- chatbot_dataset_tools/__init__.py
+++ chatbot_dataset_tools/__init__.py
@@ -1,5 +1,9 @@
-import chatbot_dataset_tools.core
-import chatbot_dataset_tools.adapters
-import chatbot_dataset_tools.transforms
+from . import core
+from . import adapters
+from . import transforms
+from . import generator
+from . import io
+from . import renderers
+from . import api
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
